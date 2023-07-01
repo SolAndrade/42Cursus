ac7532909946e3bd330bf59a6ae3423b047cc024
SolAndrade <solnicandrade@gmail.com>
2023-07-01 19:40:21 +0200

    [UPDATED] Minitalk

--- core/minitalk/client.c
+++ core/minitalk/client.c
@@ -1,13 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: soandrad <soandrad@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/15 15:52:17 by soandrad          #+#    #+#             */
-/*   Updated: 2022/11/15 15:52:18 by soandrad         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+// Function to send the string to the server using signals
+void sendStringToServer(pid_t server_pid, const char* string) {
+    // Send each character of the string to the server
+    for (int i = 0; string[i] != '\0'; i++) {
+        // Determine the signal based on the character value
+        int signal = (string[i] == '0') ? SIGUSR1 : SIGUSR2;
+        
+        // Send the signal to the server process
+        if (kill(server_pid, signal) == -1) {
+            perror("Error sending signal to server");
+            exit(EXIT_FAILURE);
+        }
+        
+        // Wait for the server to acknowledge the signal
+        pause();
+    }
+}
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_pid> <string>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // Extract the server PID from the command-line arguments
+    pid_t server_pid = atoi(argv[1]);
+
+    // Send the string to the server
+    sendStringToServer(server_pid, argv[2]);
+
+    return EXIT_SUCCESS;
+}
--- core/minitalk/server.c
+++ core/minitalk/server.c
@@ -1,12 +1,60 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: soandrad <soandrad@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/15 15:52:03 by soandrad          #+#    #+#             */
-/*   Updated: 2022/11/15 15:52:22 by soandrad         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+volatile sig_atomic_t received_signal = 0;  // Global variable to store the received signal
+
+// Signal handler for SIGUSR1
+void handleSIGUSR1(int signum) {
+    received_signal = SIGUSR1;
+}
+
+// Signal handler for SIGUSR2
+void handleSIGUSR2(int signum) {
+    received_signal = SIGUSR2;
+}
+
+// Function to receive and display the string from the client
+void receiveStringFromClient() {
+    char received_string[100];
+    int index = 0;
+
+    while (1) {
+        // Wait for the client to send a signal
+        pause();
+
+        // Check the received signal
+        if (received_signal == SIGUSR1) {
+            received_string[index++] = '0';
+        } else if (received_signal == SIGUSR2) {
+            received_string[index++] = '1';
+        } else {
+            fprintf(stderr, "Received unknown signal\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Reset the received_signal variable for the next iteration
+        received_signal = 0;
+
+        // Print the received string if it is complete
+        if (received_string[index - 1] == '\0') {
+            printf("Received string: %s\n", received_string);
+            index = 0;
+        }
+    }
+}
+
+int main() {
+    // Register the signal handlers
+    signal(SIGUSR1, handleSIGUSR1);
+    signal(SIGUSR2, handleSIGUSR2);
+
+    // Display the server PID
+    printf("Server PID: %d\n", getpid());
+
+    // Start receiving strings from clients
+    receiveStringFromClient();
+
+    return EXIT_SUCCESS;
+}
